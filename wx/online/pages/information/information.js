c64fc2921d47687dc07abedb0dc93e7374ba1b37
gengyaqiang <17621310005@163.com>
2019-06-11 12:52:12 +0800

    提交

--- wx/online/pages/information/information.js
+++ wx/online/pages/information/information.js
@@ -14,7 +14,39 @@ Page({
     indicatorDots: true,
     autoplay: true,
     interval: 5000,
-    duration: 500
+    duration: 500,
+    courseList: [
+      {
+        courseDescribe: "",
+        courseId: "47e0b0d129ea4968b8a5645c16bf5d2a",
+        courseName: "新编商务英语",
+        courseDate:"2019-6-11",
+        teacherId: "dongbo",
+        teacherName: "董波",
+        joinChapterName: "第一讲:商务英语的由来",
+        topPicSrc: "http://118.24.120.43:8080/group1/M00/00/06/rBsADFzFVPWAHFgZAAE-NPUKEQM835.jpg"
+      },
+      {
+        courseDescribe: "",
+        courseId: "47e0b0d129ea4968b8a5645c16bf5d2a",
+        courseName: "电子商务基础与应用",
+        courseDate: "2019-6-11",
+        teacherId: "dongbo",
+        teacherName: "董波",
+        joinChapterName: "第二讲:FLASH动画的原理",
+        topPicSrc: "/img/zbook.png"
+      },
+      {
+        courseDescribe: "",
+        courseId: "47e0b0d129ea4968b8a5645c16bf5d2a",
+        courseName: "计算机基础教程",
+        courseDate: "2019-6-11",
+        teacherId: "dongbo",
+        teacherName: "董波",
+        joinChapterName: "第五讲:CPU，内存的作用",
+        topPicSrc: "/img/7.jpg"
+      }
+    ]
   },
 
   /**
