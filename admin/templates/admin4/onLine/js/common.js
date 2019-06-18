72bb68f2dde625c429bd6a9d1187d23d8b7beb98
zhaomenghao <825249116@qq.com>
2019-06-18 17:52:39 +0800

    edit

--- admin/templates/admin4/onLine/js/common.js
+++ admin/templates/admin4/onLine/js/common.js
@@ -1,14 +1,14 @@
 var common = {
 	
 	//正式服
-	url: 'https://e.hnfts.cn/education',
-	url2: 'https://e.hnfts.cn/quiz',
-	uploadUrl: 'https://e.hnfts.cn/upload/upload',
+//	url: 'https://e.hnfts.cn/education',
+//	url2: 'https://e.hnfts.cn/quiz',
+//	uploadUrl: 'https://e.hnfts.cn/upload/upload',
 
 	//测试服
-//	url: 'http://192.168.10.2:8080',
-//	url2: 'http://192.168.10.2:8081',
-//	uploadUrl: 'http://192.168.10.2:8612/upload',
+	url: 'http://192.168.10.2:8080',
+	url2: 'http://192.168.10.2:8081',
+	uploadUrl: 'http://192.168.10.2:8612/upload',
 
 	pageSize: 10,
 
