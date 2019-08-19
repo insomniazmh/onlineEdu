a2cb23bff31b07ab86187eebe8f800151ae53f14
zhaomenghao <825249116@qq.com>
2019-08-19 18:34:25 +0800

    完成财务明细模块功能

--- admin/templates/admin4/train/js/common.js
+++ admin/templates/admin4/train/js/common.js
@@ -1,17 +1,14 @@
 var common = {
 	
 	//正式服
-	// url: 'https://e.hnfts.cn/education',
-	// url2: 'https://e.hnfts.cn/quiz',
-	uploadUrl: 'https://e.hnfts.cn/upload/upload',
+	// url: 'https://e.hnfts.cn/lineEdu',
 
 	//测试服
-// 	url: 'http://192.168.10.2:7080',
-// 	url2: 'http://192.168.10.2:8081',
-// 	uploadUrl: 'http://192.168.10.2:8612/upload',
+	url: 'http://192.168.10.2:7080',
 	
-	url: 'http://114.115.174.243:7080',
-	url2: 'http://114.115.174.243:8081',
+	// url: 'http://114.115.174.243:7080',
+	
+	uploadUrl: 'https://e.hnfts.cn/upload/upload',
 
 	//提示信息
 	toast: function(settings) {
