d42eac19e48f9ede496de950eeca258a686e2ae0
zhaomenghao <825249116@qq.com>
2019-11-20 13:04:06 +0800

    完成教师彻底删除课程功能，并修改url链接

--- admin/templates/admin4/onLine/js/common.js
+++ admin/templates/admin4/onLine/js/common.js
@@ -1,11 +1,11 @@
 var common = {
 	//正式服
-	 // url: 'https://e.hnfts.cn/lineEdu',
-	 // url2: 'https://e.hnfts.cn/lineEduQuiz',
+	 url: 'https://e.hnfts.cn/lineEdu',
+	 url2: 'https://e.hnfts.cn/lineEduQuiz',
 
 	//测试服
-	url: 'http://192.168.10.2:7080',
-	url2: 'http://192.168.10.2:7081',
+	// url: 'http://192.168.10.2:7080',
+	// url2: 'http://192.168.10.2:7081',
 	
 	uploadUrl: 'https://e.hnfts.cn/upload/upload',
 	// uploadUrl: 'http://192.168.10.2:8612/upload',
